84076ddb62ede6f3d43904f5d2bfdbb1da35cbe8
SunoUi <125848793+SunoUi@users.noreply.github.com>
2023-02-25 01:23:41 -0700

    Create Files.lua

---
+++ Files.lua
@@ -0,0 +1,7 @@
+makefolder('_solar-utilities')
+makefolder('_solar-utilities\\variables')
+
+writefile('_solar-utilities\\ReadMe.txt', 'Solar©️ Founded and Developed by Suno#2182, has all ownership over Solar.\n\nConfiguring with Solar files may disrupt the process of Solar and can break the script, depending on what you edit\n\nThese files will also contain your personal content like your Login information, and your Hardware-ID, if you do not want those to be shown, I recommend leaving Solar file system when streaming / recording to avoid any leaks of your personal information. Also make sure nobody else goes on your computer and looks at these documents in person.')
+
+writefile('_solar-utilities\\variables\\version.lua', '0.0.8')
+writefile('_solar-utilities\\variables\\discord.lua', 'discord.gg/9p9EWXZ6rq')
